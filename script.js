29f1771b018f33c0899beae36bc90875ef0d0ec4
ArturRibeiroDev <sk8arturzinho@gmail.com>
2026-03-05 21:39:39 -0300

    Projeto Finalizado!!!

---
+++ script.js
@@ -0,0 +1,22 @@
+let nome_do_motorista = prompt('Qual o nome do motorista? ')
+let velocidade_max = parseInt(prompt('Qual a velocidade máxima permitida na via? '))
+let velocidade_veiculo = parseInt(prompt('Qual a velocidade registrada do veículo? '))
+
+let excesso_velocidade = velocidade_veiculo - velocidade_max 
+
+if (velocidade_veiculo <= velocidade_max) {
+    console.log(`O motorista ${nome_do_motorista} está dentro do limite de velocidade. Nenhuma multa aplicada.`)
+}
+
+else if (excesso_velocidade <= 10) {
+    console.log(`O motorista ${nome_do_motorista} estava ${excesso_velocidade}Km/H acima do limite de velocidade de ${velocidade_max}Km/H e será aplicado uma multa leve no valor de: R$50`)
+}
+
+else if (excesso_velocidade > 10 && excesso_velocidade <= 20 ) {
+    console.log(`O motorista ${nome_do_motorista} estava ${excesso_velocidade}Km/H acima do limite de velocidade de ${velocidade_max}Km/H e será aplicado uma multa média no valor de: R$100`)
+}
+
+else {
+    console.log(`O motorista ${nome_do_motorista} estava ${excesso_velocidade}Km/H acima do limite de velocidade de ${velocidade_max}Km/H e será aplicado uma multa grave no valor de: R$200`)
+}
+
