f38a758b865a557753e333e7a5c9358e08ed25d0
ArturRibeiroDev <sk8arturzinho@gmail.com>
2026-03-05 22:07:16 -0300

    Projeto Finalizadissimo!!!

--- script2.js
+++ script2.js
@@ -1,13 +1,16 @@
 let nome_do_aluno = prompt('Qual o nome do aluno? ')
 let nota_prova_1= parseInt(prompt('Qual a nota da AV1? '))
 let nota_prova_2 = parseInt(prompt('Qual a nota da AV2? '))
-let nota_prova_3 = parseInt(prompt('Qual a nota da AV3? '))
-let percentual_frequencia = parseInt(prompt('Qual o percentual de frequencia do aluno? '))
+
+
 
 let nota_parcial_aluno = (nota_prova_1 + nota_prova_2) / 2
-let nota_final = (nota_parcial_aluno + nota_prova_3) / 2
+
 
 if (nota_parcial_aluno >= 4) {
+    let nota_prova_3 = parseInt(prompt('Qual a nota da AV3? '))
+    let nota_final = (nota_parcial_aluno + nota_prova_3) / 2
+    let percentual_frequencia = parseInt(prompt('Qual o percentual de frequencia do aluno? '))
     if (percentual_frequencia < 75 && nota_final >= 5){
         console.log('Você foi reprovado por falta...')
     }
