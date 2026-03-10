3b1b4b9f6b3872b510ba44dcf13ff6fcb42bd999
Artur Ribeiro Vieira Lima <109690783+ArturRibeiroDev@users.noreply.github.com>
2026-03-10 09:43:46 -0300

    Ajustando outro detalhe

--- script3.js
+++ script3.js
@@ -1,7 +1,8 @@
 let idade = parseInt(prompt('Qual a idade do usuário? '))
-let tempo_contribuição = parseInt(prompt('Qual o tempo de contribuição do usuário? '))
 
 if (idade >= 60) {
+    let tempo_contribuição = parseInt(prompt('Qual o tempo de contribuição do usuário? '))
+    
     if(idade >= 65 || tempo_contribuição >= 30) {
         console.log('O usuário está apto para se aposentar')
     }
