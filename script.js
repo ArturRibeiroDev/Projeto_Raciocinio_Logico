754001f2b1fe394c94093a8144ad3ed2f86c4b98
Artur Ribeiro Vieira Lima <109690783+ArturRibeiroDev@users.noreply.github.com>
2026-03-10 10:10:12 -0300

    Ajustando detalhes

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 let nome_do_motorista = prompt('Qual o nome do motorista? ')
 let velocidade_max = parseInt(prompt('Qual a velocidade máxima permitida na via? '))
-let velocidade_veiculo = parseInt(prompt('Qual a velocidade registrada do veículo? '))
+let velocidade_veiculo = parseFloat(prompt('Qual a velocidade registrada do veículo? '))
 
 let excesso_velocidade = velocidade_veiculo - velocidade_max 
 
 if (velocidade_veiculo <= velocidade_max) {
+    console.log(`Velocidade Registrada: ${velocidade_veiculo}km/h`)
     console.log(`O motorista ${nome_do_motorista} está dentro do limite de velocidade. Nenhuma multa aplicada.`)
 }
 
 else if (excesso_velocidade <= 10) {
-    console.log(`O motorista ${nome_do_motorista} estava ${excesso_velocidade}Km/H acima do limite de velocidade de ${velocidade_max}Km/H e será aplicado uma multa leve no valor de: R$50`)
+    console.log(`Velocidade Registrada: ${velocidade_veiculo}km/h`)
+    console.log(`O motorista ${nome_do_motorista} estava ${excesso_velocidade}km/h acima do limite de velocidade de ${velocidade_max}km/h e será aplicado uma multa leve no valor de: R$50`)
 }
 
 else if (excesso_velocidade > 10 && excesso_velocidade <= 20 ) {
-    console.log(`O motorista ${nome_do_motorista} estava ${excesso_velocidade}Km/H acima do limite de velocidade de ${velocidade_max}Km/H e será aplicado uma multa média no valor de: R$100`)
+    console.log(`Velocidade Registrada: ${velocidade_veiculo}km/h`)
+    console.log(`O motorista ${nome_do_motorista} estava ${excesso_velocidade}km/h acima do limite de velocidade de ${velocidade_max}km/h e será aplicado uma multa média no valor de: R$100`)
 }
 
 else {
-    console.log(`O motorista ${nome_do_motorista} estava ${excesso_velocidade}Km/H acima do limite de velocidade de ${velocidade_max}Km/H e será aplicado uma multa grave no valor de: R$200`)
+    console.log(`Velocidade Registrada: ${velocidade_veiculo}km/h`)
+    console.log(`O motorista ${nome_do_motorista} estava ${excesso_velocidade}km/h acima do limite de velocidade de ${velocidade_max}km/h e será aplicado uma multa grave no valor de: R$200`)
 }
 
